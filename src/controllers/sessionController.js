37b83cff6c1ab561c7273b2dcd145943e99196c3
Juan Camilo Convers Yañez <juanconvers.legal@gmail.com>
2024-05-14 10:58:45 -0500

    Commit No. 3: Implementación mejoras en la arquitectura y autorización solicitada en consigna

---
+++ src/controllers/sessionController.js
@@ -0,0 +1,61 @@
+import passport from "passport";
+
+export const login = async (req, res) => {
+    try {
+        if(!req.user){
+            return res.status(401).send("Usuario o contraseña invalidos")
+        }
+        req.session.user = {
+            email: req.user.email,
+            first_name: req.user.first_name
+        }
+        res.status(200).send("Usuario logueado correctamente")
+    } catch (e) {
+        res.status(500).send("Error al loguear usuario")
+    }
+}
+
+export const register = async (req, res) => {
+    try {
+        if(!req.user){
+            return res.status(400).send("El usuario ya existe en la app")
+        }
+        res.status(200).send("Usuario creado adecuadamente")
+    } catch (e) {
+        res.status(500).send("Error al registrar al usuario")
+    }
+}
+
+export const logout = async (req, res) => {
+    req.session.destroy(function (e) {
+        if (e) {
+            console.log(e)
+        } else {
+            res.status(200).redirect("/")
+        }
+    })
+}
+
+export const sessionGithub = async (req, res) => {
+    console.log(req)
+    req.session.user = {
+        email: req.user.email,
+        first_name: req.user.name
+    }
+    res.redirect('/')
+}
+ 
+export const testJWT = async (req, res) => {
+    if (req.user.rol == 'User')
+    res.status(403).send("Usuario no autorizado")
+else
+    res.status(200).send(req.user)
+}
+
+
+
+// sessionRouter.get('/current', passport.authenticate('jwt'), (req, res) => {
+//     console.log(req)
+//     res.status(200).send("Usuario Logueado Perfectamente")
+// })
+
--- src/routes/sessionRouter.js
+++ src/routes/sessionRouter.js
@@ -1,62 +1,20 @@
 import { Router } from "express";
 import passport from "passport";
-
+import { login, register, sessionGithub, logout, testJWT } from "../controllers/sessionController.js";
 const sessionRouter = Router()
 
-sessionRouter.get('/login', passport.authenticate('login'), async (req, res) => {
-    try {
-        if(!req.user){
-            return res.status(401).send("Usuario o contraseña invalidos")
-        }
-        req.session.user = {
-            email: req.user.email,
-            first_name: req.user.first_name
-        }
-        res.status(200).send("Usuario logueado correctamente")
-    } catch (e) {
-        res.status(500).send("Error al loguear usuario")
-    }
-})
 
-sessionRouter.post('/register', passport.authenticate('register'), async (req, res) => {
-    try {
-        if(!req.user){
-            return res.status(400).send("El usuario ya existe en la app")
-        }
-        res.status(200).send("Usuario creado adecuadamente")
-    } catch (e) {
-        res.status(500).send("Error al registrar al usuario")
-    }
-})
-sessionRouter.get('/github', passport.authenticate('github', { scope: ['user:email'] }), async (req, res) => { r })
+sessionRouter.get('/login', passport.authenticate('login'), login)
 
-sessionRouter.get('/githubSession', passport.authenticate('github'), async (req, res) => {
-    console.log(req)
-    req.session.user = {
-        email: req.user.email,
-        first_name: req.user.name
-    }
-    res.redirect('/')
-})
+sessionRouter.post('/register', passport.authenticate('register'), register)
+
+sessionRouter.get('/github', passport.authenticate('github', { scope: ['user:email'] }), async (req, res) => { })
  
-sessionRouter.get('/current', passport.authenticate('jwt'), (req, res) => {
-    console.log(req)
-    res.status(200).send("Usuario Logueado Perfectamente")
-})
+sessionRouter.get('/githubSession', passport.authenticate('github'), sessionGithub)
 
-sessionRouter.get('/logout', (req, res) => {
-    req.session.destroy(function (e) {
-        if (e) {
-            console.log(e)
-        } else {
-            res.status(200).redirect("/")
-        }
-    })
-})
+sessionRouter.get('/logout', logout)
 
-sessionRouter.get('/testJWT', passport.authenticate('jwt', { session: false }), (req, res) => {
-    res.status(200).send(req.user)
-})
+sessionRouter.get('/testJWT', passport.authenticate('jwt', { session: false }), testJWT)
 
 export default sessionRouter
 
